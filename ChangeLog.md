6f896628563f0932bb432ce661ccd5fa39b169a0
winterland1989 <winterland1989@gmail.com>
2016-11-17 19:57:30 +0800

    first (maybe the last) commit

---
+++ ChangeLog.md
@@ -0,0 +1,5 @@
+# Revision history for if
+
+## 0.1.0.0  -- 2016-11-17
+
+* First version. Released on an unsuspecting world.
---
+++ If.hs
@@ -0,0 +1,44 @@
+{-|
+Module      : If
+Description : Simple if operators
+Copyright   : (c) Winterland, 2016
+License     : BSD
+Maintainer  : drkoster@qq.com
+Stability   : stable
+Portability : PORTABLE
+
+This module provides two simple operators '?' and '?>' for simple conditions.
+-}
+
+module If where
+
+import Control.Monad (when)
+
+-- | This is the <https://wiki.haskell.org/If-then-else if-then-else> operator,
+-- With it you can write @xxx ? yyy $ zzz@ instead of @if xxx then yyy else zzz@.
+-- Following may or may not be cleaner to you ; )
+--
+-- @
+--   isFoo <- checkFoo
+--   isFoo ? foo
+--         $ bar
+-- @
+--
+(?) :: Bool -> a -> a -> a
+(?) True  x _ = x
+(?) False _ y = y
+infixr 1 ?
+{-# INLINABLE (?) #-}
+
+-- | This is the <https://mail.haskell.org/pipermail/libraries/2014-April/022700.html whenM> operator,
+-- With it you can write @doesItExists ?> removeIt@ instead of @do {e <- doesItExists; when e removeIt}@.
+-- There's not @unlessM@ version, so you have to use 'not'.
+--
+-- @
+--   not \<$\> doesItExists ?> createIt
+-- @
+--
+(?>) :: Monad m => m Bool -> m () -> m ()
+mb ?> x = mb >>= \ b -> when b x
+infixl 1 ?>
+{-# INLINABLE (?>) #-}
